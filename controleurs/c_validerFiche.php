c4eb750e18c93f891b1bef89e3f7b2fdd3c3f2d5
marie <mpersoneni@opium.eu>
2018-11-16 14:50:55 +0100

    Réorganisation des actions du switch

--- controleurs/c_validerFiche.php
+++ controleurs/c_validerFiche.php
@@ -12,41 +12,41 @@
  * @version   GIT: <0>
  * @link      http://www.siteEntreprise
  */
-
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+$idVisiteur = filter_input(INPUT_POST, 'lstVisiteurs', FILTER_SANITIZE_STRING);
+$mois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
+$lesVisiteurs = $pdo->getLesVisiteurs();
+$lesMois = getLesMois();
+$visiteurASelectionner = $idVisiteur;
+$moisASelectionner = $mois;
+include 'vues/v_listeVisiteurs.php';
 switch ($action) {
     case 'selectionnerVisiteur':
-        $lesVisiteurs = $pdo->getLesVisiteurs();
-        $lesClesVisiteurs = array_keys($lesVisiteurs);
-        $visiteurASelectionner = $lesClesVisiteurs[0];
-        $idVisiteur = $lesVisiteurs[0]['id'] ;
-        xdebug_break();
-        $lesMois = getLesMois();
-        $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
-        // Afin de sélectionner par défaut le dernier mois dans la zone de liste
-        // on demande toutes les clés, et on prend la première,
-        // les mois étant triés décroissants
-        $lesClesMois = array_keys($lesMois);
-        $moisASelectionner = $lesClesMois[0];
-        include 'vues/v_listeVisiteurs.php';   
-        break;
-        
+        $idVisiteur = $lesVisiteurs[0]['id'];
+        $moisA = $lesMois[0]['mois'];
+        //include 'vue//v_listeVisiteurs.php';   
+        break;        
     case 'voirFicheFrais': 
-        $idVisiteur = filter_input(INPUT_POST, 'lstVisiteurs', FILTER_SANITIZE_STRING);
-        $lesVisiteurs = $pdo->getLesVisiteurs();
-        $visiteurASelectionner = $idVisiteur;
-        $leMois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
-        $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
-        $moisASelectionner = $leMois;       
-        $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $leMois);
-        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $leMois);
-        $lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idVisiteur, $leMois);
-        $numAnnee = substr($leMois, 0, 4);
-        $numMois = substr($leMois, 4, 2);
+        $moisASelectionner = $mois;       
+        $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $mois);
+        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $mois);
+        $lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idVisiteur, $mois);
+        $numAnnee = substr($mois, 0, 4);
+        $numMois = substr($mois, 4, 2);
         $nbJustificatifs = $lesInfosFicheFrais['nbJustificatifs'];
-        if ($lesInfosFicheFrais){
+        if($lesInfosFicheFrais){
             $dateModif = dateAnglaisVersFrancais($lesInfosFicheFrais['dateModif']);
         }    
-        include 'vues/v_listeVisiteurs.php';
+        //include 'vues/v_listeVisiteurs.php';
         include 'vues/v_validerFicheFrais.php';
+        break;
+    case 'validerMajFraisForfait':
+        $lesFrais = filter_input(INPUT_POST, 'lesFrais', FILTER_DEFAULT, FILTER_FORCE_ARRAY);
+        if (lesQteFraisValides($lesFrais)) {
+            $pdo->majFraisForfait($idVisiteur, $mois, $lesFrais);
+        } else {
+            ajouterErreur('Les valeurs des frais doivent être numériques');
+            include 'vues/v_erreurs.php';
+        }
+        break;
 }
