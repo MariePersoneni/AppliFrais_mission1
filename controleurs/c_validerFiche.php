765d94b0ba57a1950c93cf4c6f40c7841c4a9b33
marie <mpersoneni@opium.eu>
2018-11-16 15:03:57 +0100

    Réorganisation

--- controleurs/c_validerFiche.php
+++ controleurs/c_validerFiche.php
@@ -13,21 +13,22 @@
  * @link      http://www.siteEntreprise
  */
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
-$idVisiteur = filter_input(INPUT_POST, 'lstVisiteurs', FILTER_SANITIZE_STRING);
-$mois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
 $lesVisiteurs = $pdo->getLesVisiteurs();
 $lesMois = getLesMois();
-$visiteurASelectionner = $idVisiteur;
-$moisASelectionner = $mois;
-include 'vues/v_listeVisiteurs.php';
+
 switch ($action) {
     case 'selectionnerVisiteur':
         $idVisiteur = $lesVisiteurs[0]['id'];
-        $moisA = $lesMois[0]['mois'];
-        //include 'vue//v_listeVisiteurs.php';   
+        $mois = $lesMois[0]['mois'];
+        $visiteurASelectionner = $idvisiteur;
+        $moisASelectionner = $mois;
+        include 'vues/v_listeVisiteurs.php';
         break;        
     case 'voirFicheFrais': 
-        $moisASelectionner = $mois;       
+        $idVisiteur = filter_input(INPUT_POST, 'lstVisiteurs', FILTER_SANITIZE_STRING);
+        $mois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
+        $visiteurASelectionner = $idvisiteur;
+        $moisASelectionner = $mois;
         $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $mois);
         $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $mois);
         $lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idVisiteur, $mois);
@@ -37,13 +38,19 @@ switch ($action) {
         if($lesInfosFicheFrais){
             $dateModif = dateAnglaisVersFrancais($lesInfosFicheFrais['dateModif']);
         }    
-        //include 'vues/v_listeVisiteurs.php';
+        include 'vues/v_listeVisiteurs.php';
         include 'vues/v_validerFicheFrais.php';
         break;
     case 'validerMajFraisForfait':
+        $idVisiteur = filter_input(INPUT_POST, 'lstVisiteurs', FILTER_SANITIZE_STRING);
+        $mois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
+        $visiteurASelectionner = $idvisiteur;
+        $moisASelectionner = $mois;
         $lesFrais = filter_input(INPUT_POST, 'lesFrais', FILTER_DEFAULT, FILTER_FORCE_ARRAY);
         if (lesQteFraisValides($lesFrais)) {
             $pdo->majFraisForfait($idVisiteur, $mois, $lesFrais);
+            include 'vues/v_listeVisiteurs.php';
+            include 'vues/v_validerFicheFrais.php';
         } else {
             ajouterErreur('Les valeurs des frais doivent être numériques');
             include 'vues/v_erreurs.php';
