6a77f9c55dc754742f91c5ef8f4865b311cf1b33
marie <mpersoneni@opium.eu>
2018-11-16 16:58:35 +0100

    Création d'une vue pour afficher un message lorsque la modification d'une fiche de frais est un succès.

---
+++ vues/v_modificationEffectuee.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Vue Modification effectu�e sur fiche de frais
+ *
+ * PHP Version 7
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    Réseau CERTA <contact@reseaucerta.org>
+ * @author    José GIL <jgil@ac-nice.fr>
+ * @copyright 2017 Réseau CERTA
+ * @license   Réseau CERTA
+ * @version   GIT: <0>
+ * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
+ */
+?>
+<div class="alert alert-success" role="alert">
+    <?php
+    echo '<p>' . 'Modification effectuée' . '</p>';
+    ?>
+</div>
