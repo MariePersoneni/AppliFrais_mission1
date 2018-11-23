b24b9d3c22bacd65171904fb04ad393426d37cd1
marie <mpersoneni@opium.eu>
2018-11-23 16:19:42 +0100

    Modification de l'affichage des frais kilométriques Calcul automatique du montant remboursé selon la puissance du véhicule utilisé (voir pour enregistrer ces infos dans la BDD : puissance véhicule) Changement des couleurs comptable pour que les nuances soient les mêmes que la version visiteur.

--- vues/v_validerFicheFrais.php
+++ vues/v_validerFicheFrais.php
@@ -39,10 +39,40 @@ if ($lesInfosFicheFrais) {
                         ?>
                         <div class="form-group">
         					<label for="idFrais"><?php echo $libelle ?></label> 
-        					<input
-        						type="text" id="idFrais" name="lesFrais[<?php echo $idFrais ?>]"
-        						size="10" maxlength="5" value="<?php echo $quantite ?>"
-        						class="form-control">
+        					
+        					<?php 
+                            if ($idFrais == 'KM') {
+                            ?>
+                            <br>
+                            <label for="lstTypeehicule" class="sous-label">Puissance véhicule</label>
+                            <select type="text" id="lstTypeVehicule" name="lstTypeVehicule"
+                                   class="form-control" onchange="calculMontantKM()"> 
+                                   <option selected value = "0.52">4CV Diesel</option>
+                                   <option value = "0.58">5/6CV Diesel</option>
+                                   <option value = "0.62">4CV Essence</option>
+                                   <option value = "0.67">5/6CV Essence</option>
+                            </select>
+                            <label for="qteKM" class="sous-label">Kilomètres parcourus</label>
+                            <input type="text" id="qteKM" name="qteKM"
+                                   size="10" maxlength="5" 
+                                   value="<?php echo $quantite ?>" 
+                                   class="form-control" onchange="calculMontantKM()">
+                           <label for="montantKM" class="sous-label">Montant pris en charge</label>
+                           <input type="text" id="montantKM" name="montantKM"
+                                   size="10" maxlength="5"  
+                                   class="form-control">
+                           
+    						<?php 
+                            } else {
+                           ?>
+                           <input type="text" id="idFrais" 
+                                   name="lesFrais[<?php echo $idFrais ?>]"
+                                   size="10" 
+                                   value="<?php echo $quantite ?>" 
+                                   class="form-control">
+    						<?php 
+                            } 
+                            ?>
     					</div>
                     <?php
                     }
@@ -146,5 +176,16 @@ if ($lesInfosFicheFrais) {
     ?>
     <h3>Pas de fiche de frais pour ce visiteur pour ce mois.</h3>
     <?php 
-    }
-    ?>
+}
+
+print("<SCRIPT language=javascript>");
+print(
+    "function calculMontantKM() {
+        var coeffVoiture = document.getElementById(\"lstTypeVehicule\").value;
+        var nbKM = document.getElementById(\"qteKM\").value;
+        var montantKM = Math.round(coeffVoiture * nbKM);
+        document.getElementById(\"montantKM\").value = montantKM + \" euros\";
+    }"
+    );
+print("</script>");
+?>
